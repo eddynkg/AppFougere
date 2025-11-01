a83aa68285a299b5ed84d3e6585553f4eed86367
Basile Carle <basile.carle@edhec.com>
2025-11-01 07:02:01 +0100

    - ajout d'un champ de complétion d'adresse avec suggestion pour récupérer une localisation - affichage d'une carte (pas encore reliée à l'adresse recherchée

--- AppFougere/View/Subviews/AddActivityComponents/AddLocationComponent.swift
+++ AppFougere/View/Subviews/AddActivityComponents/AddLocationComponent.swift
@@ -8,19 +8,33 @@
 import SwiftUI
 import CoreLocation
 import CoreLocationUI
+import MapKit
 import Observation
 
 struct AddLocationComponent: View {
     @State var locationManager = LocationManager()
+    @State var searchCompleter = SearchCompleter()
+    @State var locationSearch: String = ""
+    @State var areSuggestionDisplayed: Bool = false
+    @State var isMapDisplayed: Bool = false
     
-  
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 43.61091, longitude: 3.87630),
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    )
+    
+    var pinLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(
+        latitude: 43.61091,
+        longitude: 3.87630
+    )
+
     var body: some View {
         VStack {
             HStack {
                 Text("Localisation : ")
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .customBody(bold: true, color: .capVerde)
-                if let loc = locationManager.location {
+                if locationManager.location != nil {
                     Text("Position enregistrée")
                         .customText(bold: false, fontSize: 10, color: .capVerde)
                         .frame(width: 110, alignment: .center )
@@ -34,6 +48,34 @@ struct AddLocationComponent: View {
                 .tint(.capVerde)
                 
             }
+            TextField("Adresse de l'activité", text: $locationSearch)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .onChange(
+                    of: locationSearch
+                ) {
+                    areSuggestionDisplayed = true
+                    searchCompleter.updateResults(for: locationSearch)
+                }
+            if areSuggestionDisplayed {
+                VStack {
+                    ForEach(searchCompleter.results, id: \.self) { result in
+                        Text(result)
+                            .onTapGesture {
+                                locationSearch = result
+                                areSuggestionDisplayed = false
+                            }
+                    }
+                }
+            }
+            Map{
+                Marker("Montpellier", coordinate: pinLocation)
+            }.mapControlVisibility(.visible)
+                .frame(height: 300)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 16)
+                )
+                .padding()
         }
         
     }
