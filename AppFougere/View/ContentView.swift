b96067ce607c98be82c0b354c5eba9a001c49523
Basile Carle <basile.carle@edhec.com>
2025-11-05 16:37:29 +0100

    Fixed contentview redirecting to HomeView instead of tabbarview

--- AppFougere/View/ContentView.swift
+++ AppFougere/View/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
 
     var body: some View {
         if session.isLoggedIn {
-            HomeView()
+            TabBarView()
         } else {
             AuthView()
         }
